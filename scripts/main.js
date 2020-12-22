4bfb8aa368202f9622a4636d034dc3c49d9fc391
Joao Fortunato <joaomlfortunato@gmail.com>
2020-12-22 12:02:39 +0000

    Add Operate Function
    Calls Add, Subtract, Multuply or Divide depending on the operator;
    Only works for numbers (does not accept strings of numbers).

--- scripts/main.js
+++ scripts/main.js
@@ -18,3 +18,21 @@ const add = (number1, number2) => number1 + number2;
 const subtract = (number1, number2) => number1 - number2;
 const multiply = (number1, number2) => number1 * number2;
 const divide = (numerator, denominator) => numerator/denominator;
+
+function operate(number1, number2, operator) {
+    if (typeof(number1) !== "number" || typeof(number2) !== "number") {
+        return undefined
+    }
+    switch (operator) {
+        case "+":
+            return add(number1,number2)
+        case "-":
+            return subtract(number1, number2)
+        case "*":
+            return multiply(number1, number2)
+        case "/":
+            return divide(number1, number2)
+        default:
+            return undefined
+    }
+}
