a7d3e33573a1830c981c00a2158103baa07e42a6
Joao Fortunato <joaomlfortunato@gmail.com>
2020-12-23 17:05:46 +0000

    Change main.js Structure
    Functions first, then local variables, then event listeners.

--- scripts/main.js
+++ scripts/main.js
@@ -1,78 +1,4 @@
 (function() {
-
-    let primaryDisplayNumber = "";
-    const maxPrimaryDisplayLength = 10;
-    let hasDecimalPoint = false;
-    let result = undefined;
-    let operator = undefined;
-
-    const pageBody = document.querySelector("body");
-    const darkThemeToggle = document.querySelector("#dark-theme-input");
-    const keysContainer = document.querySelector("#btns-container");
-    const primaryDisplay = document.querySelector("#primary-display");
-    const secundaryDisplay = document.querySelector("#secundary-display");
-    const keys = document.querySelectorAll("button");
-    const decimalButton = document.querySelector("#decimal");
-
-    let isOnDarkTheme = false;
-    darkThemeToggle.addEventListener("click", () => {
-        if (!isOnDarkTheme) {
-            isOnDarkTheme = true;
-            pageBody.classList.add("dark-theme");
-        }
-        else {
-            isOnDarkTheme = false;
-            pageBody.classList.remove("dark-theme");
-        }
-
-    }, false);
-
-    keysContainer.addEventListener("click", e => {
-        if (e.target.classList.contains("number-keys")) {
-            if (result !== undefined && operator === undefined) {
-                result = undefined;
-                resetUI();
-            }
-            else if (primaryDisplayNumber === "ERROR")
-            {
-                resetUI();
-            }
-            writePrimaryDisplay(e.target);
-        }
-        else if (e.target.classList.contains("operators")) {
-            if (result === undefined && operator === undefined && primaryDisplayNumber !== "ERROR") {
-                operator = e.target.value;
-                result = parseFloat(primaryDisplayNumber);
-                resetUI();
-            }
-            else if (result !== undefined && operator === undefined){
-                operator = e.target.value;
-            }
-            else if (result !== undefined && operator !== undefined) {
-                result = operate(result,parseFloat(primaryDisplayNumber),operator);
-                operator = e.target.value;
-                resetUI();
-                primaryDisplay.textContent = result;
-                errorHandling(result);
-            }        
-        }
-        else if (e.target.id === "equal") {
-            if (result !== undefined && operator !== undefined) {
-                result = operate(result,parseFloat(primaryDisplayNumber),operator);
-                resetUI();
-                primaryDisplay.textContent = result;
-                operator = undefined;
-                errorHandling(result);
-            }
-        }
-        else if (e.target.id === "clear-all") {
-            clearAll();
-        }
-        else if (e.target.classList.contains("backspace")) {
-            backspace();
-        }
-    }, false);
-
     const add = (number1, number2) => number1 + number2;
     const subtract = (number1, number2) => number1 - number2;
     const multiply = (number1, number2) => number1 * number2;
@@ -105,7 +31,7 @@
         return parseFloat(operationResult.toPrecision(maxPrimaryDisplayLength));
     }
 
-    function writePrimaryDisplay (numberButton) {
+    function writePrimaryDisplayNumber (numberButton) {
         if (primaryDisplayNumber.length >= maxPrimaryDisplayLength) {
             return;
         }
@@ -190,4 +116,79 @@
             primaryDisplay.textContent = "ERROR";
         }
     }
+
+    let primaryDisplayNumber = "";
+    const maxPrimaryDisplayLength = 10;
+    let hasDecimalPoint = false;
+    let result = undefined;
+    let operator = undefined;
+
+    const pageBody = document.querySelector("body");
+    const darkThemeToggle = document.querySelector("#dark-theme-input");
+    const keysContainer = document.querySelector("#btns-container");
+    const primaryDisplay = document.querySelector("#primary-display");
+    const secundaryDisplay = document.querySelector("#secundary-display");
+    const keys = document.querySelectorAll("button");
+    const decimalButton = document.querySelector("#decimal");
+
+    let isOnDarkTheme = false;
+
+    darkThemeToggle.addEventListener("click", () => {
+        if (!isOnDarkTheme) {
+            isOnDarkTheme = true;
+            pageBody.classList.add("dark-theme");
+        }
+        else {
+            isOnDarkTheme = false;
+            pageBody.classList.remove("dark-theme");
+        }
+
+    }, false);
+
+    keysContainer.addEventListener("click", e => {
+        if (e.target.classList.contains("number-keys")) {
+            if (result !== undefined && operator === undefined) {
+                result = undefined;
+                resetUI();
+            }
+            else if (primaryDisplayNumber === "ERROR")
+            {
+                resetUI();
+            }
+            writePrimaryDisplayNumber(e.target);
+        }
+        else if (e.target.classList.contains("operators")) {
+            if (result === undefined && operator === undefined && primaryDisplayNumber !== "ERROR") {
+                operator = e.target.value;
+                result = parseFloat(primaryDisplayNumber);
+                resetUI();
+            }
+            else if (result !== undefined && operator === undefined){
+                operator = e.target.value;
+            }
+            else if (result !== undefined && operator !== undefined) {
+                result = operate(result,parseFloat(primaryDisplayNumber),operator);
+                operator = e.target.value;
+                resetUI();
+                primaryDisplay.textContent = result;
+                errorHandling(result);
+            }        
+        }
+        else if (e.target.id === "equal") {
+            if (result !== undefined && operator !== undefined) {
+                result = operate(result,parseFloat(primaryDisplayNumber),operator);
+                resetUI();
+                primaryDisplay.textContent = result;
+                operator = undefined;
+                errorHandling(result);
+            }
+        }
+        else if (e.target.id === "clear-all") {
+            clearAll();
+        }
+        else if (e.target.classList.contains("backspace")) {
+            backspace();
+        }
+    }, false);
+    
 })();
